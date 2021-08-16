6168527e06fef505fe11f6e6c9193f6c26937a15
eafedorow <61786122+eafedorow@users.noreply.github.com>
2021-08-16 19:18:27 +0300

    Начальная версия проекта
    Функционал:
    1. Создание постов (Заголовок и содержимое).
    2. Удаление постов

---
+++ src/components/Counter.jsx
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import ClassCounter from "./ClassCounter";
+
+const Counter = () => {
+
+    const [count, setCount] = useState(0);
+
+
+
+    return (
+        <div className='App'>
+
+        </div>
+
+    );
+};
+
+export default Counter;
